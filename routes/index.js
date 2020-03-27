dd18f50e83922711ad4cd73ef9e772633345fa4a
Shinjan Mitra <shinjanxp@gmail.com>
2020-03-27 20:43:50 +0530

    feat: Creates a ppe request submit page
    Basic front-end layout for a submit request page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var models  = require('../models');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/ppe/create', function (req, res, next) {
+  res.sendfile('./public/ppe-create.html');
+});
+
+router.post('/ppe', function (req, res, next) {
+  console.log(req.body);
+  res.sendfile('./public/ppe-create.html');
+})
 
 module.exports = router;
