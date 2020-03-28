319f2e9ff64e679fe5c09a16e2f74509967b1f6a
Shinjan Mitra <shinjanxp@gmail.com>
2020-03-28 14:07:20 +0530

    config: Changes production config to use sqlite

--- config/config.js
+++ config/config.js
@@ -8,11 +8,7 @@ module.exports = {
       storage: ":memory:"
     },
     production: {
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      host: process.env.DB_HOSTNAME,
-      dialect: 'mysql',
-      use_env_variable: 'DATABASE_URL'
+      dialect: "sqlite",
+      storage: "./db.development.sqlite"
     }
   };
